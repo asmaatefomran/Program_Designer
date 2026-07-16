c0797e4b58d7338a57d9dd56b1f2d8ca2d38f1b5
Asmaa Atef <asmaatef6789@gmail.com>
2026-07-16 13:12:53 +0300

    feat: finish program management endpoints

---
+++ src/ProgramDesigner.Api/Controllers/ProgramsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramDesigner.Api.DTOs.Requests;
+using ProgramDesigner.Api.Services.Interfaces;
+using ProgramDesigner.Api.DTOs.Responses;
+
+namespace ProgramDesigner.Api.Controllers;
+
+[ApiController]
+[Route("programs")]
+public class ProgramsController : ControllerBase
+{
+   private readonly IProgramService _programService;
+
+    public ProgramsController(
+        IProgramService programService)
+    {
+        _programService = programService;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<CreateProgramResponse>> Create([FromBody]CreateProgramRequest request)
+    {
+        var result =  await _programService.CreateAsync(request);
+        return CreatedAtAction(nameof(GetById), new {id = result.Program.Id}, result);
+    }
+    
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProgramResponse>> GetById(string id)
+    {
+        var program = await _programService.GetAsync(id);
+        return program is null? NotFound() : Ok(program);
+    }
+    
+    
+    [HttpPost("{id}/validate")]
+    public async Task<ActionResult<ValidationResponse>> Validate(string id)
+    {
+        var validation = await  _programService.ValidateAsync(id);
+        return validation is null ? NotFound() : Ok(validation);
+    }
+    
+}
--- src/ProgramDesigner.Api/Program.cs
+++ src/ProgramDesigner.Api/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ProgramDesigner.Api.Data;
+using ProgramDesigner.Api.Services;
+using ProgramDesigner.Api.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Register DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProgramBuilderService, ProgramBuilderService>();
+builder.Services.AddScoped<IValidationService, ValidationService>();
+builder.Services.AddScoped<IProgramLoaderService, ProgramLoaderService>();
+builder.Services.AddScoped<IProgramService, ProgramService>();
 
 // Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
@@ -9,14 +24,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
