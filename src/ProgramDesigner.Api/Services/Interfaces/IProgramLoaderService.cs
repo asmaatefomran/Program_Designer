278b50afea01da3d65b4129f4cd2dd198c0a4779
Asmaa Atef <asmaatef6789@gmail.com>
2026-07-16 13:11:35 +0300

    feat: implement recursive program loader

---
+++ src/ProgramDesigner.Api/Services/Interfaces/IProgramLoaderService.cs
@@ -0,0 +1,8 @@
+﻿using ProgramDesigner.Api.Domain.Entities;
+
+namespace ProgramDesigner.Api.Services.Interfaces;
+
+public interface IProgramLoaderService
+{
+    Task<LearningProgram?> LoadProgramAsync(string programId);
+}
---
+++ src/ProgramDesigner.Api/Services/ProgramLoaderService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramDesigner.Api.Data;
+using ProgramDesigner.Api.Domain.Entities;
+using ProgramDesigner.Api.Services.Interfaces;
+
+namespace ProgramDesigner.Api.Services;
+
+public class ProgramLoaderService : IProgramLoaderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProgramLoaderService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<LearningProgram?> LoadProgramAsync(string programId)
+    {
+        var program = await _context.Programs
+            .FirstOrDefaultAsync(p => p.Id == programId);
+
+        if (program is null)
+            return null;
+
+        program.RootGroup = await LoadGroupAsync(program.RootGroupId);
+
+        return program;
+    }
+    
+    private async Task<Group> LoadGroupAsync(string groupId)
+    {
+        var group = await _context.Groups
+            .Include(g => g.Children)
+            .FirstAsync(g => g.Id == groupId);
+
+        await LoadPrerequisites(group);
+
+        await LoadChildren(group);
+
+        return group;
+    }
+    
+    private async Task LoadChildren(Group group)
+    {
+        var children = new List<Node>();
+
+        foreach (var child in group.Children)
+        {
+            children.Add(await LoadNodeAsync(child.Id));
+        }
+
+        group.Children = children.OrderBy(c => c.OrderIndex).ToList();
+    }
+    
+    private async Task<Node> LoadNodeAsync(string nodeId)
+    {
+        var node = await _context.Nodes
+            .FirstAsync(n => n.Id == nodeId);
+
+        await LoadPrerequisites(node);
+
+        if (node is Group group)
+        {
+            await LoadChildren(group);
+        }
+
+        return node;
+    }
+    
+    private async Task LoadPrerequisites(Node node)
+    {
+        node.Prerequisites = await _context.NodePrerequisites
+            .Where(p => p.NodeId == node.Id)
+            .Include(p => p.Prerequisite)
+            .ToListAsync();
+    }
+
+}
