7b637430a5204b73d618a324379a2aec40a3110d
Asmaa Atef <asmaatef6789@gmail.com>
2026-07-18 18:34:08 +0300

    created new dtos to handle paganation and getting the lates list of programs

---
+++ src/ProgramDesigner.Api/DTOs/Responses/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgramDesigner.Api.DTOs.Responses;
+
+public class PagedResult<T>
+{
+    public required List<T> Items { get; init; }
+    public required int TotalCount { get; init; }
+    public required int Page { get; init; }
+    public required int PageSize { get; init; }
+    public int TotalPages => PageSize == 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+}
---
+++ src/ProgramDesigner.Api/DTOs/Responses/ProgramSummaryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgramDesigner.Api.DTOs.Responses;
+
+
+public class ProgramSummaryResponse
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required DateTime CreatedAt { get; init; }
+}
